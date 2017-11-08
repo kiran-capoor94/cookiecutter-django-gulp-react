17340488a630ffbf973cd6db4f01a8aab5c8eb63
Bjorn Kristinsson <bjorn@bjornkri.com>
2017-11-08 15:57:57 +0100

    Set up linting, ignores

--- {{cookiecutter.project_slug}}/gulpfile.js
+++ {{cookiecutter.project_slug}}/gulpfile.js
@@ -13,33 +13,43 @@ const babelify = require('babelify');
 const { reload, stream } = browserSync;
 
 // Relative paths function
-var pathsConfig = function (appName) {
-  this.app = "./" + (appName || pjson.name);
+const pathsConfig = (appName) => {
+  this.app = `./${(appName || pjson.name)}`;
 
   return {
     app: this.app,
-    css: this.app + '/static/css',
+    css: `${this.app}/static/css`,
     scss: './app/scss',
-    js: this.app + '/static/js',
+    js: `${this.app}/static/js`,
     jsx: './app/js',
     templates: `${this.app}/templates`,
-  }
+  };
 };
 
-var paths = pathsConfig('{{cookiecutter.project_slug}}');
+const paths = pathsConfig('{{cookiecutter.project_slug}}');
 
-
-gulp.task('javascript', function () {
+// Compile js changes
+gulp.task('javascript', () => {
   gulp.src('./app/js/app.jsx')
+    .pipe(plumber())
     .pipe(bro({
-        transform: [
-          babelify.configure({ presets: ['env', 'react'] })
-        ]
+      transform: [
+        babelify.configure({ presets: ['env', 'react'] }),
+      ],
     }))
     .pipe(rename('project.js'))
-    .pipe(gulp.dest(paths.js))
+    .pipe(gulp.dest(paths.js));
 });
 
+
+// Compile sass changes
+gulp.task('scss', () => {
+  gulp.src(`${paths.scss}/*.scss`)
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest(paths.css))
+    .pipe(stream());
+});
 
 // create a task that ensures the `javascript` task is complete before
 // reloading browsers
@@ -49,29 +59,15 @@ gulp.task('js-watch', ['javascript'], (done) => {
 });
 
 // Run django server
-gulp.task('runServer', function(cb) {
-  var cmd = spawn('python', ['manage.py', 'runserver', '0.0.0.0:8002'], {stdio: 'inherit'});
-  cmd.on('close', function(code) {
-    console.log('runServer exited with code ' + code);
-    cb(code);
-  });
+gulp.task('runServer', (cb) => {
+  const cmd = spawn('python', ['manage.py', 'runserver', '0.0.0.0:8002'], { stdio: 'inherit' });
+  cmd.on('close', code => cb(code));
 });
 
 
 // Browser sync server for live reload
-gulp.task('browserSync', function() {
-    browserSync.init(
-      [paths.css + "/*.css", paths.js + "*.js", paths.templates + '*.html'], {
-        proxy:  "localhost:8002"
-    });
-});
-
-// Watch for sass changes
-gulp.task('scss', () => {
-  gulp.src(`${paths.scss}/*.scss`)
-    .pipe(sass())
-    .pipe(gulp.dest(paths.css))
-    .pipe(stream());
+gulp.task('browserSync', () => {
+  browserSync.init({ proxy: 'localhost:8002' });
 });
 
 
@@ -84,6 +80,4 @@ gulp.task('watch', () => {
 
 
 // Default task
-gulp.task('default', function() {
-  runSequence(['javascript', 'scss'], ['runServer', 'browserSync', 'watch']);
-});
+gulp.task('default', () => runSequence(['javascript', 'scss'], ['runServer', 'browserSync', 'watch']));
