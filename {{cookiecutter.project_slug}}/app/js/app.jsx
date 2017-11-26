9b568da566648ca377cc8343ff8a08b63b790038
Bjorn Kristinsson <bjorn@bjornkri.com>
2017-11-26 15:52:57 +0100

    Add optional bootstrap support

--- {{cookiecutter.project_slug}}/app/js/app.jsx
+++ {{cookiecutter.project_slug}}/app/js/app.jsx
@@ -1,3 +1,12 @@
+{% if cookiecutter.use_bootstrap == 'y' %}const jQuery = require('jquery');
+const Popper = require('popper.js');
+// export for others scripts to use
+window.$ = jQuery;
+window.jQuery = jQuery;
+window.Popper = Popper;
+
+require('bootstrap');
+{% endif %}
 import React from 'react';
 import ReactDOM from 'react-dom';
 
